8821dca74c286f3fb8b4d1635116ea505f7ac4e0
LASS <l-a-s-s@outlook.com>
2023-05-15 22:02:02 -0600

    trabajo en pausa mejorando el codigo

---
+++ js/app.js
@@ -0,0 +1,13 @@
+import { valida } from "./validaciones";
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+    input.addEventListener("blur", (input) => {
+        valida(input.target);
+    });
+})
+
+const colores = ["rojo", "verde", "blanco"];
+console.log(colores);
+// colores.forEach(())
--- js/validaciones.js
+++ js/validaciones.js
@@ -1,12 +1,23 @@
-const inputNacimiento = document.querySelector("#birth");
+export function valida(input){
+    // el dataset lo que hace es tomar todo los dataset existentes y el .tipo es como agarrar el que definimos en html "data-tipo"
+    const tipoDeInput = input.dataset.tipo;
+    // Recuerda que los objetos son valores compuestos que puede tener o asiganar a una cosntante y dentro tienen keys o llaves y cada key se puede clasificar como atributos/propiedades que son como otras constantes y las funciones serian metodos que necsitan los () para ejecutarla, por eso validadores[tipoDeInput](input) es similar a validadores.tipoDeInput(input); ref: https://youtu.be/4xig5UPRC00
+    if (validadores[tipoDeInput]){
+        validadores[tipoDeInput](input);
+    }
+}
+
+const validadores = {
+    nacimiento: (input) => validarNacimiento(input)
+};
+
+// const inputNacimiento = document.querySelector("#birth");
 // console.log(inputNacimiento);
-
-
 // Blur es cuando hace click e introduce los datos y despues de salirse o hacer click en otro lado manda a ejecutar la funcion
-inputNacimiento.addEventListener("blur", (evento) => {
-    validarNacimiento(evento.target);
-    // console.log(evento);
-})
+// inputNacimiento.addEventListener("blur", (evento) => {
+//     validarNacimiento(evento.target);
+//     // console.log(evento);
+// })
 
 // Esta funcion valida Nacimiento tiene el parametro input que se hace llamar en inputNacimiento en funcion al target que es donde estan todos los parametros del objeto asi donde este input sera por ejemplo evento.target.value del objeto #birth
 function validarNacimiento(input){
